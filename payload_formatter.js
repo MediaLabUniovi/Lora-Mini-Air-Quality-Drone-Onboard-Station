7403db29de073a8ecdd9e33c4a7eb386fa4ad02e
MediaLab_ LPWAN <146118367+medialablpwan@users.noreply.github.com>
2023-12-13 01:13:29 +0100

    Update payload_formatter.js to BETA

--- payload_formatter.js
+++ payload_formatter.js
@@ -8,12 +8,12 @@ function decodeUplink(input) {
 
   data.temperature = (bytes[0] | (bytes[1] << 8)) / 100;
   data.humidity = bytes[2] | (bytes[3] << 8);
-  data.pressure = (((bytes[4])) + ((bytes[5]) << 8) + ((bytes[6]) << 16) + ((bytes[7]) << 24)) / 100;
-  data.battery = (bytes[8] | (bytes[9] << 8)) / 1000;
-  data.co2 = bytes[10] | (bytes[11] << 8);
-  data.tvoc = bytes[14] | (bytes[13] << 8);
+  data.pressure = (bytes[4]) + (bytes[5] + (bytes[6] << 8)) / 1000;
+  data.battery = (bytes[7] | (bytes[8] << 8)) / 1000;
+  data.co2 = bytes[9] | (bytes[10] << 8);
+  data.tvoc = bytes[11] | (bytes[12] << 8);
 
-    return { 
+    return {
       data: data,
       warnings: [],
       errors: []
