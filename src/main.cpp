e96496a5e3335fbc17135afda9f36dc0d9ce41d1
MediaLab_ LPWAN <146118367+medialablpwan@users.noreply.github.com>
2023-12-13 13:14:47 +0100

    Update main.cpp to first release!

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 #include <Seeed_BME280.h>
 #include <CCS811.h>                                                         // HelTec modified version of Adafruit_CCS811 library (inlcuded in HelTec documentation)
 #include <Wire.h>
-#include "ttnvalues.h"                                                      // OTAA keys to synchronize with TTN
+#include "credentials.h"                                                    // OTAA keys to synchronize with TTN
 
-#define PA_ATM 101300
+#define PA2ATM 101300
 
-bool ENABLE_DEBUG = true;                                                   // Boolean toggle to enable serial monitor
-uint32_t appTxDutyCycle = 10000;                                            // Data transmission rate time (in milliseconds)
+bool ENABLE_DEBUG = false;                                                  // Boolean toggle to enable serial monitor
+uint32_t appTxDutyCycle = 90000;                                            // Data transmission rate time (in milliseconds)
 
 uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
 LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;                              // Macro that represents the region, EU868
@@ -21,7 +21,7 @@ uint8_t appPort = 2;
 uint8_t confirmedNbTrials = 4;                                              // Trials of TX
 
 // Sensors variables -----------------------------------------------------------------------------------------
-int temperature, humidity, pressure_int, pressure_dec, batteryVoltage;
+int temperature, humidity, pressure_int, pressure_dec, batteryLevel;
 long pressure_pa;
 float pressure_atm;
 uint16_t co2_i = 0, tvoc_i = 0, co2_medio, tvoc_medio;
@@ -50,22 +50,30 @@ void ccs_medio(){
     }
     delay(1000);
   }
-  Serial.print("El CO2 medio es: ");
   co2_medio = co2_medio / 5;
-  Serial.print("El TVOC medio es: ");
+  Serial.print("El CO2 medio es: ");
+  Serial.println(co2_medio);
   tvoc_medio = tvoc_medio / 5;
+  Serial.print("El TVOC medio es: ");
+  Serial.println(tvoc_medio);
 }
 
 // Function to divide pressure into integer and decimal parts -----------------------------------------------
 void pressure_parts(){
   float dec;
-  Serial.print("La presion en Pa");
+  Serial.print("La presion en Pa ");
   Serial.print(pressure_pa);
-  pressure_atm = pressure_pa / PA_ATM;                                      // Pressure in atm (too many decimals)
+  pressure_atm = pressure_pa / PA2ATM;                                      // Pressure in atm (too many decimals)
+  Serial.print("La presion en atm es: ");
+  Serial.println(pressure_atm);
   pressure_int = floor(pressure_atm);                                       // Integer part of atms
+  Serial.print("Parte entera de la presion en atm: ");
+  Serial.println(pressure_int);
   dec = pressure_atm - pressure_int;                                        // Decimal part of atms
   dec *= 1000;                                                              // Number of decimals I want
   pressure_dec = dec;                                                       // Decimals I want as an int and without residual decimal numbers
+  Serial.print("Parte decimal de la presion en atm: ");
+  Serial.println(pressure_dec);
 }
 
 // Function to prepare the transmission =====================================================================
@@ -73,10 +81,10 @@ static void prepareTxFrame(uint8_t port){
   // This enables the output to power the sensor ------------------------------------------------------------
   pinMode(Vext, OUTPUT);                                                    // Vext is a reserved word to refer to the 3.3V pin of the CubeCell board
   digitalWrite(Vext, LOW);
-	pinMode(GPIO0, OUTPUT);
-	digitalWrite(GPIO0, LOW);
-	pinMode(GPIO4, OUTPUT);                                                   // RGB LED GPIO
-	digitalWrite(GPIO4, HIGH);
+  pinMode(GPIO0, OUTPUT);
+  digitalWrite(GPIO0, LOW);
+  pinMode(GPIO4, OUTPUT);                                                   // RGB LED GPIO
+  digitalWrite(GPIO4, HIGH);
   delay(500);
 
   Serial.println("BME280 and CCS811 CubeCell test ALPHA");
@@ -86,9 +94,7 @@ static void prepareTxFrame(uint8_t port){
 
   // If BME does not init, print error if serial enabled ----------------------------------------------------
   if(!bme280.init()){
-    if(ENABLE_DEBUG){
-      Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    }
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
   }
 
   delay(500);                                                               // This delay is required to allow the sensor time to init
@@ -103,10 +109,8 @@ static void prepareTxFrame(uint8_t port){
 
   // If CCS does not begin, print an error if serial enabled ------------------------------------------------
   if(!ccs.begin()){
-     if(ENABLE_DEBUG){
-       Serial.println("Failed to start sensor! Please check your wiring.");
-       while(1);
-    }
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
   }
 
   while(!ccs.available());                                                  // Wait for the sensor to be ready
@@ -119,10 +123,10 @@ static void prepareTxFrame(uint8_t port){
   
   // Turn the power to the sensor off again ----------------------------------------------------------------
   digitalWrite(Vext, HIGH);
-	digitalWrite(GPIO0,HIGH);
-	digitalWrite(GPIO4,LOW);
+  digitalWrite(GPIO0,HIGH);
+  digitalWrite(GPIO4,LOW);
 
-  batteryVoltage = getBatteryVoltage();                                     // CubeCell built-in function to get battery voltage
+  batteryLevel = (BoardGetBatteryLevel() / 254) * 100;                      // CubeCell built-in function to get battery voltage
 
   // TX bytes array initialization --------------------------------------------------------------------------
   appDataSize = 13;
@@ -136,32 +140,29 @@ static void prepareTxFrame(uint8_t port){
   appData[5] = lowByte(pressure_dec);
   appData[6] = highByte(pressure_dec);
 
-  appData[7] = lowByte(batteryVoltage);
-  appData[8] = highByte(batteryVoltage);
+  appData[7] = lowByte(batteryLevel);
 
-  appData[9] = lowByte(co2_medio);
-  appData[10] = highByte(co2_medio);
+  appData[8] = lowByte(co2_medio);
+  appData[9] = highByte(co2_medio);
 
-  appData[11] = lowByte(tvoc_medio);
-  appData[12] = highByte(tvoc_medio);
+  appData[10] = lowByte(tvoc_medio);
+  appData[11] = highByte(tvoc_medio);
 
   // Serial monitor messages to debug -----------------------------------------------------------------------
-  if(ENABLE_DEBUG){
-    Serial.print("Temperature: ");
-    Serial.print(temperature / 100);
-    Serial.print("C, Humidity: ");
-    Serial.print(humidity);
-    Serial.print("%, Pressure: ");
-    Serial.print(pressure_atm);
-    Serial.print(" atm, Battery Voltage: ");
-    Serial.print(batteryVoltage);
-    Serial.println(" mV");
+  Serial.print("Temperature: ");
+  Serial.print(temperature / 100);
+  Serial.print("C, Humidity: ");
+  Serial.print(humidity);
+  Serial.print("%, Pressure: ");
+  Serial.print(pressure_atm);
+  Serial.print(" atm, Battery Voltage: ");
+  Serial.print(batteryLevel);
+  Serial.println(" %");
 
-    Serial.print("CO2: ");
-    Serial.print(co2_medio);
-    Serial.print("ppm, TVOC: ");
-    Serial.println(tvoc_medio);
-  }
+  Serial.print("CO2: ");
+  Serial.print(co2_medio);
+  Serial.print("ppm, TVOC: ");
+  Serial.println(tvoc_medio);
 }
 
 // 'setup()' function =======================================================================================
@@ -179,11 +180,11 @@ void setup(){
 // 'loop()' function ========================================================================================
 void loop(){
   // CubeCell LoRaWAN state machine -------------------------------------------------------------------------
-  Serial.println("Inicializando maquina de estados...");
+  //Serial.println("Inicializando maquina de estados...");
   switch(deviceState){
 
     // State that indicates the process of joining a LoRaWAN network ----------------------------------------
-    Serial.println("Buscando red LoRaWAN");
+    //Serial.println("Buscando red LoRaWAN");
     case DEVICE_STATE_INIT:
     {
       printDevParam();
@@ -193,7 +194,7 @@ void loop(){
     }
 
     // State that indicates the process of having joined a LoRaWAN network -----------------------------------
-    Serial.println("Uniéndome a una red LoRaWAN");
+    //Serial.println("Uniéndome a una red LoRaWAN");
     case DEVICE_STATE_JOIN:
     {
       LoRaWAN.join();                                                       // Function to join the network
@@ -201,7 +202,7 @@ void loop(){
     }
 
     // Automatically change to send state --------------------------------------------------------------------
-    Serial.println("Preparando bytes a enviar");
+    //Serial.println("Preparando bytes a enviar");
     case DEVICE_STATE_SEND:
     {
       prepareTxFrame(appPort);
@@ -211,7 +212,7 @@ void loop(){
     }
 
     // State that indicates the process of timing the data transmission rate --------------------------------
-    Serial.println("Temporizando duty cycle");
+    //Serial.println("Temporizando duty cycle");
     case DEVICE_STATE_CYCLE:
     {
       txDutyCycleTime = appTxDutyCycle + randr(0, APP_TX_DUTYCYCLE_RND);    // Schedule next packet transmission. 'APP_TX_DUTYCYCLE_RND' defines a random delay for application data transmission duty cycle. 1s
@@ -221,7 +222,7 @@ void loop(){
     }
 
     // State that indicates the process of starting deep sleep between cycles -------------------------------
-    Serial.println("Me piro a dormir!");
+    //Serial.println("Me piro a dormir!");
     case DEVICE_STATE_SLEEP:
     {
       LoRaWAN.sleep();                                                      // Function to activate deep sleep for the timed time
