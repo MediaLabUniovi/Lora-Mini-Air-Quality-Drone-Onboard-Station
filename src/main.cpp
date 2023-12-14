ededa077517b38955b95028261038324a0a2ddbb
MediaLab_ LPWAN <146118367+medialablpwan@users.noreply.github.com>
2023-12-14 13:54:19 +0100

    Update main.cpp to v1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+/* ***********************************************************************************************************
+SENSOR ALI - MEDIALAB LPWAN UNIVERSIDAD DE OVIEDO
+
+Este archivo ha sido modificado de manera considerable para implementar el sensor de CO2 y particulas
+volatiles, además del original BME280. El enlace para consultar el proyecto original del usuario de GitHub
+"alexcorvis84" es el siguiente: https://github.com/alexcorvis84/mini-lora-weatherstation.git
+*********************************************************************************************************** */
+
+// 'includes' for libraries and header files -----------------------------------------------------------------
 #include <LoRaWan_APP.h>                                                    // HelTec LoRa library
 #include <Arduino.h>
 #include <Seeed_BME280.h>
@@ -5,11 +14,14 @@
 #include <Wire.h>
 #include "credentials.h"                                                    // OTAA keys to synchronize with TTN
 
-#define PA2ATM 101300
+// 'defines' for various aims --------------------------------------------------------------------------------
+#define ENABLE_DEBUG 0                                                      // Toggle to enable/disable serial monitor
+#define PA2ATM 101300                                                       // Conversion factor from Pa to atm (pressure)
+#define CCS8_I 5                                                            // Amount of iterations to make an average for the CCS811 sensor values
+                                                
+uint32_t appTxDutyCycle = 90000;                                            // Data transmission rate time (in milliseconds). Cannot be declared and initialized in other ways as it refers to a library variable
 
-bool ENABLE_DEBUG = false;                                                  // Boolean toggle to enable serial monitor
-uint32_t appTxDutyCycle = 90000;                                            // Data transmission rate time (in milliseconds)
-
+// LoRaWAN technic declarations -----------------------------------------------------------------------------
 uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
 LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;                              // Macro that represents the region, EU868
 DeviceClass_t  loraWanClass = LORAWAN_CLASS;                                // Macro that represents the device class, A
@@ -21,10 +33,11 @@ uint8_t appPort = 2;
 uint8_t confirmedNbTrials = 4;                                              // Trials of TX
 
 // Sensors variables -----------------------------------------------------------------------------------------
-int temperature, humidity, pressure_int, pressure_dec, batteryLevel;
+int temperature, pressure_int, pressure_dec, batteryVoltage;
+uint8_t humidity, batteryLevel, level;
 long pressure_pa;
 float pressure_atm;
-uint16_t co2_i = 0, tvoc_i = 0, co2_medio, tvoc_medio;
+uint16_t co2_i, tvoc_i, co2_medio, tvoc_medio;
 
 // Sensors constructors -------------------------------------------------------------------------------------
 BME280 bme280;
@@ -33,8 +46,10 @@ CCS811 ccs;
 // Function to get the average measurement of CO2 and TVOC out of 5 individual measurements -----------------
 void ccs_medio(){
   co2_medio = 0;
+  co2_i = 0;
   tvoc_medio = 0;
-  for(int i = 0; i < 5; i++){
+  tvoc_i = 0;
+  for(int i = 0; i < CCS8_I; i++){
     if(!ccs.readData()){                                                    // 'ccs.readData()' is kinda tricky. It returns a true if no measurements are available, and false if it successfully found something to show. That is why it partnered with a not (!)
       co2_i = ccs.geteCO2();                                                // Both 'ccs.getCO2()' and 'ccs.getTVOC()' return the values
       co2_medio += co2_i;
@@ -50,10 +65,10 @@ void ccs_medio(){
     }
     delay(1000);
   }
-  co2_medio = co2_medio / 5;
+  co2_medio /= CCS8_I;
   Serial.print("El CO2 medio es: ");
   Serial.println(co2_medio);
-  tvoc_medio = tvoc_medio / 5;
+  tvoc_medio /= CCS8_I;
   Serial.print("El TVOC medio es: ");
   Serial.println(tvoc_medio);
 }
@@ -76,15 +91,22 @@ void pressure_parts(){
   Serial.println(pressure_dec);
 }
 
+// Function to get battery level from the built-in battery voltage function ---------------------------------
+uint8_t getLiPolevel(){
+  batteryVoltage = getBatteryVoltage();
+  level = (0.009 * batteryVoltage - 3.3);
+  return level;
+}
+
 // Function to prepare the transmission =====================================================================
 static void prepareTxFrame(uint8_t port){
   // This enables the output to power the sensor ------------------------------------------------------------
   pinMode(Vext, OUTPUT);                                                    // Vext is a reserved word to refer to the 3.3V pin of the CubeCell board
-  digitalWrite(Vext, LOW);
-  pinMode(GPIO0, OUTPUT);
-  digitalWrite(GPIO0, LOW);
-  pinMode(GPIO4, OUTPUT);                                                   // RGB LED GPIO
-  digitalWrite(GPIO4, HIGH);
+  digitalWrite(Vext, LOW);                                                  // SET IT TO LOW MEANS ENABLING 3.3V, IT WORKS "INVERSELY"
+	pinMode(GPIO0, OUTPUT);                                                   // GPIO0 is used to control the 'WAK' pin of the CCS, enabling and disabling sleep mode of the sensor
+	digitalWrite(GPIO0, LOW);                                                 // It is set to LOW when reading as "GND" makes it be awake. Kinda contra-intuitive, I know
+	pinMode(GPIO4, OUTPUT);                                                   // RGB LED GPIO
+	digitalWrite(GPIO4, HIGH);
   delay(500);
 
   Serial.println("BME280 and CCS811 CubeCell test ALPHA");
@@ -122,31 +144,31 @@ static void prepareTxFrame(uint8_t port){
   Wire.end();
   
   // Turn the power to the sensor off again ----------------------------------------------------------------
-  digitalWrite(Vext, HIGH);
-  digitalWrite(GPIO0,HIGH);
-  digitalWrite(GPIO4,LOW);
+  digitalWrite(Vext, HIGH);                                                 // SET IT TO HIGH, DISABLING 3.3V OUTPUT, REMEMBER IS CONTRA-INTUITIVE
+	digitalWrite(GPIO0,HIGH);                                                 // CCS 'WAK' pin control to HIGH as this enables the sleep mode
+	digitalWrite(GPIO4,LOW);
 
-  batteryLevel = (BoardGetBatteryLevel() / 254) * 100;                      // CubeCell built-in function to get battery voltage
+  //batteryLevel = (BoardGetBatteryLevel() / 254) * 100;                      // CubeCell built-in function to get battery level
+  batteryLevel = getLiPolevel();                                            // Self-made function to get battery level as the one above does not work properly
 
   // TX bytes array initialization --------------------------------------------------------------------------
-  appDataSize = 13;
+  appDataSize = 11;
   appData[0] = lowByte(temperature);                                        // As temperature values range from bigger than 256 (more than a byte) and less than 65536 (less than 2 bytes), functions 'lowByte' and 'highByte' are needed
   appData[1] = highByte(temperature);
 
   appData[2] = lowByte(humidity);
-  appData[3] = highByte(humidity);
 
-  appData[4] = lowByte(pressure_int);
-  appData[5] = lowByte(pressure_dec);
-  appData[6] = highByte(pressure_dec);
+  appData[3] = lowByte(pressure_int);
+  appData[4] = lowByte(pressure_dec);
+  appData[5] = highByte(pressure_dec);
 
-  appData[7] = lowByte(batteryLevel);
+  appData[6] = lowByte(batteryLevel);
 
-  appData[8] = lowByte(co2_medio);
-  appData[9] = highByte(co2_medio);
+  appData[7] = lowByte(co2_medio);
+  appData[8] = highByte(co2_medio);
 
-  appData[10] = lowByte(tvoc_medio);
-  appData[11] = highByte(tvoc_medio);
+  appData[9] = lowByte(tvoc_medio);
+  appData[10] = highByte(tvoc_medio);
 
   // Serial monitor messages to debug -----------------------------------------------------------------------
   Serial.print("Temperature: ");
@@ -169,7 +191,7 @@ static void prepareTxFrame(uint8_t port){
 void setup(){
   // ONLY the initialization of the CubeCell board ----------------------------------------------------------
   boardInitMcu();
-  if(ENABLE_DEBUG){
+  if(ENABLE_DEBUG == 1){
     Serial.begin(115200);                                                   // It is here where serial is initialized
   }
 
@@ -180,11 +202,9 @@ void setup(){
 // 'loop()' function ========================================================================================
 void loop(){
   // CubeCell LoRaWAN state machine -------------------------------------------------------------------------
-  //Serial.println("Inicializando maquina de estados...");
   switch(deviceState){
 
     // State that indicates the process of joining a LoRaWAN network ----------------------------------------
-    //Serial.println("Buscando red LoRaWAN");
     case DEVICE_STATE_INIT:
     {
       printDevParam();
@@ -194,7 +214,6 @@ void loop(){
     }
 
     // State that indicates the process of having joined a LoRaWAN network -----------------------------------
-    //Serial.println("Uniéndome a una red LoRaWAN");
     case DEVICE_STATE_JOIN:
     {
       LoRaWAN.join();                                                       // Function to join the network
@@ -202,7 +221,6 @@ void loop(){
     }
 
     // Automatically change to send state --------------------------------------------------------------------
-    //Serial.println("Preparando bytes a enviar");
     case DEVICE_STATE_SEND:
     {
       prepareTxFrame(appPort);
@@ -212,7 +230,6 @@ void loop(){
     }
 
     // State that indicates the process of timing the data transmission rate --------------------------------
-    //Serial.println("Temporizando duty cycle");
     case DEVICE_STATE_CYCLE:
     {
       txDutyCycleTime = appTxDutyCycle + randr(0, APP_TX_DUTYCYCLE_RND);    // Schedule next packet transmission. 'APP_TX_DUTYCYCLE_RND' defines a random delay for application data transmission duty cycle. 1s
@@ -222,7 +239,6 @@ void loop(){
     }
 
     // State that indicates the process of starting deep sleep between cycles -------------------------------
-    //Serial.println("Me piro a dormir!");
     case DEVICE_STATE_SLEEP:
     {
       LoRaWAN.sleep();                                                      // Function to activate deep sleep for the timed time
